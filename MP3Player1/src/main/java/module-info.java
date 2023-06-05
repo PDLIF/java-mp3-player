3e704a571ced233e00c430b75b1a314a29e4b446
VinerX <60321176+VinerX@users.noreply.github.com>
2023-06-05 16:32:16 +0300

    Revert "Revert "Delete MP3Player1 directory""
    This reverts commit cbf7fb1bd69f3afbcb7f6a87ab4f3991095c3046.

--- MP3Player1/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.mp3player {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens com.example.mp3player to javafx.fxml;
-    exports com.example.mp3player;
-}
