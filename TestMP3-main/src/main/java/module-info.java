e3ee7001d13a20e24a20c994848a000e9257dadd
VinerX <60321176+VinerX@users.noreply.github.com>
2023-06-12 19:28:02 +0300

    SomeTests
    1) файл с тестами
    2) Геттеры в файле контроллера
    3) Новые зависимости - тестфх для тестов гуи

--- TestMP3-main/src/main/java/module-info.java
+++ TestMP3-main/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.mp3player {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires java.desktop;
 
 
     opens com.example.mp3player to javafx.fxml;
---
+++ TestMP3-main/src/test/java/com/example/mp3player/ControllerTest.java
@@ -0,0 +1,207 @@
+package com.example.mp3player;
+
+import eu.hansolo.tilesfx.addons.Switch;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.VerticalDirection;
+import javafx.scene.Node;
+import javafx.scene.control.Slider;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
+import javafx.util.Duration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.function.Try;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxRobotContext;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import org.testfx.framework.junit5.Stop;
+import org.testfx.service.finder.WindowFinder;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@ExtendWith(ApplicationExtension.class)
+class ControllerTest {
+    Stage stage;
+    Scene scene;
+    Controller controller;
+    //Desktop desktop = null;
+    @Start
+    private void start(Stage stage) throws IOException {
+        this.stage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
+        scene = new Scene(fxmlLoader.load(), Color.AQUA);
+        controller = fxmlLoader.getController();
+
+        stage.setTitle("MP3 Player");
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+    }
+
+    @Test
+    void clickOnButtonAdd(FxRobot robot) {
+        robot.clickOn("#addButton");
+        robot.push(KeyCode.TAB);
+        robot.push(KeyCode.TAB);
+        robot.push(KeyCode.TAB);
+        robot.push(KeyCode.TAB);
+        robot.push(KeyCode.ENTER);
+        robot.push(KeyCode.BACK_SPACE);
+
+
+        String p = controller.getDirectory().getAbsolutePath();
+
+        p=p.substring(0,p.length()-5);
+        System.out.println(p);
+        // Костыль так как write в либе походу сломан
+        for (String s: (p+"src/test/java/com/example/mp3player/Test Song3.mp3").toUpperCase().split(""))
+        {   //System.out.println(s);
+            switch (s) {
+
+                case "\\", "/" -> robot.push(KeyCode.BACK_SLASH);
+                case "." -> robot.push(KeyCode.PERIOD);
+                case " " -> robot.push(KeyCode.SPACE);
+                case ":" -> robot.push(KeyCode.COLON);
+                case "-" -> robot.sleep(1);
+                default -> {
+                    KeyCode k = KeyCode.getKeyCode(s);
+                    robot.push(k);
+                }
+            }
+        };
+        robot.push(KeyCode.TAB);
+        robot.push(KeyCode.TAB);
+        robot.push(KeyCode.TAB);
+        robot.push(KeyCode.TAB);
+        robot.push(KeyCode.TAB);
+        robot.push(KeyCode.TAB);
+        robot.push(KeyCode.TAB);
+
+
+        robot.push(KeyCode.ENTER);
+
+
+    }
+
+    @Test
+    void clickOnButtonDelete() {
+    }
+
+    @Test
+    void handleDragOver(FxRobot robot) throws IOException {
+
+
+        //desktop.open(new File("src/test/java/com/example/mp3player"));
+
+        //JFileChooser jFileChooser = new JFileChooser("src/test/java/com/example/mp3player");
+        //jFileChooser.
+        //System.out.println(jFileChooser.getSelectedFile().getName() );
+
+        //robot.sleep(5000);
+
+        //JFileChooser jFileChooser = new JFileChooser();
+        //System.out.println(jFileChooser.getSelectedFile().getName());
+
+    }
+
+    @Test
+    void handleDragExited(FxRobot robot) {
+    }
+
+    @Test
+    void handleDragDropped(FxRobot robot) {
+    }
+
+    @Test
+    void testClickOnButtonPlay(FxRobot robot) {
+
+        robot.clickOn("#playButton");
+        Assertions.assertEquals("PLAYING", controller.getMediaPlayer().getStatus().toString());
+
+
+
+    }
+
+    @Test
+    void testClickOnPauseMediaAfterPlay(FxRobot robot) {
+        robot.clickOn("#playButton");
+        robot.clickOn("#pauseButton");
+        Assertions.assertEquals("PAUSED", controller.getMediaPlayer().getStatus().toString());
+    }
+
+    @Test
+    void testClickOnResetMediaAfterPlay(FxRobot robot) {
+        robot.clickOn("#playButton");
+        robot.clickOn("#resetButton");
+        //Меньше секунды (погрешность)
+        Assertions.assertTrue( controller.getMediaPlayer().getCurrentTime().lessThanOrEqualTo(Duration.seconds(1)) );
+        // Слайдер на 0
+        Assertions.assertEquals(0, robot.lookup("#songProgressBar").queryAs(Slider.class).getValue() );
+    }
+
+    @Test
+    void testClickOnPreviousMediaAfterPlay(FxRobot robot) {
+        robot.clickOn("#playButton");
+        String previousSongName = robot.lookup("#songName").toString();
+        robot.clickOn("#previousButton");
+        Assertions.assertNotEquals(previousSongName,robot.lookup("#songName").toString());
+
+
+    }
+
+    @Test
+    void testClickOnNextMediaAfterPlay(FxRobot robot) {
+        robot.clickOn("#playButton");
+        String previousSongName = robot.lookup("#songName").toString();
+        robot.clickOn("#nextButton");
+        Assertions.assertNotEquals(previousSongName,robot.lookup("#songName").toString());
+    }
+
+    @Test
+    void testChangeSpeedTo25Percent(FxRobot robot) {
+
+        robot.clickOn("#speedBox");
+        robot.moveTo(robot.offset("#speedBox",0,25));
+        robot.clickOn();
+
+        Assertions.assertEquals("25%",robot.lookup("#speedBox").queryComboBox().getValue() );
+
+    }
+
+    @Test
+    void testBeginTimer(FxRobot robot) {
+
+    }
+
+    @Test
+    void testCancelTimer(FxRobot robot) {
+    }
+
+
+}
